0b3ac826fac3f55c9ce6c6812a8371d89f8eea13
Sakthi Vignesh <120655500+Sakthi-007@users.noreply.github.com>
2024-03-30 16:22:28 +0530

    Sakthi 16:22

---
+++ Server/utils/index.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+const url="mongodb+srv://sakthi:Vicky07@cluster0.ggmamcl.mongodb.net/taskDB?retryWrites=true&w=majority";
+
+const dbConnection =async ()=>{
+    try {
+        await mongoose.connect(url);
+        console.log("DB connected");
+    } catch (error) {
+        console.log("DB error"+ error);
+    }
+}
+
+export default dbConnection;
+
+export const createJWT = (RES,USERiD)=>{
+const token = jwt.sign({USERiD},process.env.JWT_SECRET,{expiresIn:'1d',
+
+});
+
+res.cookie("token",token,{
+    httpOnly:true,
+    secure:process.env.NODE_ENV !=="development",
+    sameSite:"strict",// prevent CSRF attacks
+    maxAge:1*24*60*60*1000,//! day
+
+})
+
+}
