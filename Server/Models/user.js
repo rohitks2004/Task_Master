bee82be0107e7f92d7a6d5776afb21c6deb2d0e4
Sakthi-007 <sakthisiva2004@gmail.com>
2025-02-11 18:02:35 +0530

    added .env

--- Server/Models/user.js
+++ Server/Models/user.js
@@ -9,7 +9,7 @@ const userSchema = new Schema({
     role:{type:String,required:true},
     isAdmin:{type:Boolean,required:true,default:false},
     tasks:[{type:Schema.Types.ObjectId,ref:"Task"}],
-    isActive:{type:Boolean,required:true,default:false}
+    isActive:{type:Boolean,required:true,default:true}
    
 },
 {timestamps:true}
